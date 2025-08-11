1281d5dd49cdec7c12ff802d9672dd64615d9d18
Ashutoshhere18 <mishraashutoshwk@gmail.com>
2025-08-11 21:55:39 +0530

    11/08/25

--- Text-Utils/Utils_Project/src/components/Text_Utils/TextUtils.jsx
+++ Text-Utils/Utils_Project/src/components/Text_Utils/TextUtils.jsx
@@ -4,6 +4,9 @@ export default function TextUtils() {
 const[Text,SetText]=useState("");
 const[Characters,SetCharacters]=useState(0);
 const[Words,SetWords]=useState(0);
+const[UpperCase,SetUpperCase]=useState(0);
+const[LowerCase,SetLowerCase]=useState(0);
+const[TimeTaken,SetTime]=useState(0);
 
 const HandlerText=(e)=> SetText(e.target.value);
 const HandlerCharacterCount=()=>{
@@ -14,19 +17,44 @@ const HandlerWordsCount=()=>{
  SetWords(words.length);
 }
 
-
-
+const HandlerUpperCount=()=>{
+   const count = (Text.match(/[A-Z]/g) || []).length;
+   SetUpperCase(count);
+}
+const HandlerLowerCount=()=>{
+   const count = (Text.match(/[a-z]/g) || []).length;
+   SetLowerCase(count);
+}
+const TimeHandler =()=>{ 
+   const time= Words / 200;
+    SetTime(time.toFixed(2));
+  };
 
   return (
-    <div>
-      <input type="text" placeholder='Enter Text.....' onChange={HandlerText} />
+    <div className='text-utils-container'>
+      <h1 className="page-title">📊 Text Analysis Tool</h1>
+        <p className="page-subtitle">
+        Enter your text below and click <strong>Analyze Text</strong> to get detailed statistics including total characters, words, uppercase, lowercase letters, and estimated reading time.
+      </p>
+
+      <textarea type="text" placeholder='Enter Text.....'  value={Text} onChange={HandlerText}></textarea>
         <p>{Text}</p>
-        <h2>Total Characters:{Characters}</h2>
-        <h2>Total Words:{Words}</h2>
-        <button onClick={()=>{
-            HandlerCharacterCount();
-            HandlerWordsCount();
-        }}> Count Characters & Words </button>
+         <button className='analyze-btn' onClick={()=>{
+              HandlerCharacterCount();
+          HandlerWordsCount();
+          HandlerUpperCount();
+          HandlerLowerCount();
+          TimeHandler();
+        }}> Analyse Text </button>
+             <div className="results">
+        <h2>🔍 Analysis Results</h2>
+        <p><strong>Total Characters:</strong> {Characters}</p>
+        <p><strong>Total Words:</strong> {Words}</p>
+        <p><strong>Total Uppercase Letters:</strong> {UpperCase}</p>
+        <p><strong>Total Lowercase Letters:</strong> {LowerCase}</p>
+        <p><strong>Estimated Reading Time:</strong> {TimeTaken} minutes</p>
+      </div>
+        
     </div>
   )
 }
