47476f3793fcf86b2366813338f579a5180dfb63
Ashutoshhere18 <mishraashutoshwk@gmail.com>
2025-08-21 10:18:19 +0530

    21/08/25

--- Portfolio/My-Portfolio/src/Screen/Home/Home.jsx
+++ Portfolio/My-Portfolio/src/Screen/Home/Home.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '..//../Components/Navbar/Navbar'
 import HomePage from '../../Components/HomePage/HomePage'
+import axios from 'axios'
 export default function Home() {
+
+
+const[GithubData,SetGithubData]=useState({});
+
+useEffect(()=>{
+  FetchApi().then((data)=>SetGithubData(data));
+},[]);
+
+const FetchApi=async()=>{
+const res=  await axios.get("https://api.github.com/users/Ashutoshhere18");
+ return res.data;
+}
+
   return (
     <div>
       <Navbar/>
-      <HomePage/>
+      <HomePage Data={GithubData}/>
     </div>
   )
 }
