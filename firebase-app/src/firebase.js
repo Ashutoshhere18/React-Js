aeff9f3037ff5e96527870a28dc8bff33a8dadfc
Ashutoshhere18 <mishraashutoshwk@gmail.com>
2025-11-06 08:54:30 +0530

    06/11/25

---
+++ firebase-app/src/firebase.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAZN-yxqlrISPKl69VKRLukzYglJ6BqAxA",
+  authDomain: "fir-app-f7c25.firebaseapp.com",
+  projectId: "fir-app-f7c25",
+  storageBucket: "fir-app-f7c25.firebasestorage.app",
+  messagingSenderId: "1044390592441",
+  appId: "1:1044390592441:web:8e5c53052f08d7c1024068",
+  measurementId: "G-HCXC4RR2YX"
+};
+
+
+const app = initializeApp(firebaseConfig);
