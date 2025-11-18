993fcb0e624ce614e432540da3dca056a2972cd3
Ashutoshhere18 <mishraashutoshwk@gmail.com>
2025-11-18 08:38:44 +0530

    ...

--- firebase-app/src/App.jsx
+++ firebase-app/src/App.jsx
@@ -1,8 +1,9 @@
 
 import { useState } from 'react'
 import './App.css'
-import {auth} from './firebase'
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import {auth,provider,db} from './firebase'
+import {collection,addDoc} from 'firebase/firestore'
+import { createUserWithEmailAndPassword,signInWithPopup } from 'firebase/auth';
 function App() {
 
   const [email,setEmail]=useState("");
@@ -14,11 +15,27 @@ function App() {
     .catch((error)=>alert(error.message));
  }
 
+  const handleGoogleSignIn=()=>{
+    signInWithPopup(auth,provider).then((result)=>{
+      const credential=result.user;
+      alert("Sign in Successfully!!"+credential.email);
+    }).catch(err=>err.message);
+  }
+
+  const handleFirestore=()=>{
+   addDoc(collection(db,"Todo",{
+    "Title":"Get up early",
+    "Time":"6"
+   })).then(()=>alert("Data added successfully!!")).catch(err=>alert(err.message))
+  }
+
   return (
     <>
       <input type="text" placeholder='enter email' onChange={(e)=>{setEmail(e.target.value)}} />
       <input type="text" placeholder='enter password' onChange={(e)=>{setPassword(e.target.value)}} />
       <button onClick={handleSignUp}>Sign up</button>
+      <button onClick={handleGoogleSignIn}>Sign in with google</button>
+      <button onClick={handleFirestore}>Add Data</button>
     </>
   )
 }
--- firebase-app/src/firebase.js
+++ firebase-app/src/firebase.js
@@ -1,6 +1,7 @@
 
 import { initializeApp } from "firebase/app";
-import {getAuth,createUserWithEmailAndPassword} from 'firebase/auth'
+import {getAuth,GoogleAuthProvider} from 'firebase/auth'
+import {  getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAZN-yxqlrISPKl69VKRLukzYglJ6BqAxA",
@@ -15,3 +16,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth=getAuth(app);
+export const provider=new GoogleAuthProvider();
+export const db=getFirestore(app);
