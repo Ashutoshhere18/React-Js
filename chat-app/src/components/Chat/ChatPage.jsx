44ea77e910e394b044514710ebfb2ad6a27c0cfa
Ashutoshhere18 <mishraashutoshwk@gmail.com>
2025-11-25 15:41:40 +0530

    25/11/2025

--- chat-app/src/components/Chat/ChatPage.jsx
+++ chat-app/src/components/Chat/ChatPage.jsx
@@ -1,46 +1,111 @@
 import React, { useEffect, useState } from 'react'
-import {useLocation} from 'react-router'
+import { useLocation } from 'react-router'
 import './ChatPage.css'
-import {sendMessage,readMessages} from '../../slices/ChatSlices'
-import {useDispatch,useSelector} from 'react-redux'
-import {getUser,getCurrentUser} from '../../slices/userSlices'
+import { sendMessage, readMessages, deleteMessage, updateMessage } from '../../slices/ChatSlices'
+import { useDispatch, useSelector } from 'react-redux'
+import { getUser } from '../../slices/userSlices'
 
 export default function ChatPage() {
-  const dispatch=useDispatch();
-  // const {currentUser}=useSelector(state=>state.user);
-  const currentUser=JSON.parse(localStorage.getItem("user"));
-   const {chats}=useSelector((state)=>state.chats);
-   const [message,setMessage]=useState("")
-    const location=useLocation();
-    const receiver=location.state;
+  const dispatch = useDispatch();
+  const currentUser = JSON.parse(localStorage.getItem("user"));
+  const { chats } = useSelector((state) => state.chats);
 
-    useEffect(()=>{
-      // dispatch(getCurrentUser());
-      dispatch(getUser())
-      dispatch(readMessages({sender:currentUser.email,receiver:receiver.email}))
-    },[])
+  const [message, setMessage] = useState("");
+  const [editId, setEditId] = useState("");
 
-    const HandleMessage=()=>{
-      dispatch(sendMessage({message:message,sender:currentUser.email,receiver:receiver.email}))
-       dispatch(readMessages({sender:currentUser.email,receiver:receiver.email}))
-       setMessage("")
+  const location = useLocation();
+  const receiver = location.state;
+
+  useEffect(() => {
+    dispatch(getUser());
+    dispatch(readMessages({ sender: currentUser.email, receiver: receiver.email }));
+  }, []);
+
+  const handleSendOrUpdate = () => {
+    if (editId) {
+      
+      dispatch(updateMessage({
+        sender: currentUser.email,
+        receiver: receiver.email,
+        chatId: editId,
+        newMessage: message
+      }));
+
+      setEditId("");
+      setMessage("");
+    } else {
+      
+      dispatch(sendMessage({
+        message: message,
+        sender: currentUser.email,
+        receiver: receiver.email
+      }));
+
+      setMessage("");
     }
+
+    dispatch(readMessages({ sender: currentUser.email, receiver: receiver.email }));
+  };
+
+  const handleDeleteMessage = (chat) => {
+  if (chat.sender !== currentUser.email) {
+    alert("You can delete only your own messages!");
+    return;
+  }
+
+  dispatch(deleteMessage({
+    sender: currentUser.email,
+    receiver: receiver.email,
+    chatId: chat.chatId
+  }));
+};
+
+
+  const startEditing = (chat) => {
+    if(chat.sender!=currentUser.email){
+      alert("only update your own message")
+      return
+    }
+    setEditId(chat.chatId);
+    setMessage(chat.message);
+  };
+
   return (
-   <div className="chat-view">
-  <div className="chat-page">
-    <h3>User: {receiver.email}</h3>
-    <div>
-      {
-        chats.map((chat,i)=><p key={i}>{chat.message}</p>)
-      }
-    </div>
+    <div className="chat-view">
+      <div className="chat-page">
+        <h3>User: {receiver.email}</h3>
 
-    <div className="input-area">
-      <input onChange={(e)=>setMessage(e.target.value)} value={message} type="text" />
-      <button onClick={HandleMessage}>Send</button>
-    </div>
-  </div>
-</div>
+        <div>
+          {chats.map((chat, i) => {
+            const position = chat.sender === currentUser.email
+              ? "message-end"
+              : "message-start";
 
-  )
+            return (
+              <div
+                key={i}
+                className={`message-div ${position}`}
+                onClick={() => startEditing(chat)}         
+                onDoubleClick={() => handleDeleteMessage(chat)} 
+              >
+                <span className="message-box">{chat.message}</span>
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="input-area">
+          <input
+            type="text"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+
+          <button onClick={handleSendOrUpdate}>
+            {editId ? "Update" : "Send"}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- chat-app/src/slices/ChatSlices.js
+++ chat-app/src/slices/ChatSlices.js
@@ -1,69 +1,103 @@
-import {createSlice,createAsyncThunk} from '@reduxjs/toolkit';
-import {db} from '../firebase';
-import {addDoc,doc,setDoc,getDocs,collection} from 'firebase/firestore'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { db } from '../firebase';
+import { updateDoc, doc, setDoc, getDocs, collection, deleteDoc } from 'firebase/firestore'
 
-export const readMessages=createAsyncThunk("chat/read",async({sender,receiver})=>{
-     const users=[sender,receiver];
-    users.sort()
-    const docId=users[0]+"_"+users[1];
-   try{
-     const snapshots=await getDocs(collection(db,"chatroom",docId,"chats"))
-    const chatList=snapshots.docs.map((snap)=>snap.data())
-    return chatList;
-   }catch(e){
-    console.log(e)
-   }
 
+const docIdGenerator = (sender, receiver) => {
+    const user = [sender, receiver];
+    user.sort();
+    const docId = user[0] + "_" + user[1];
+    return docId;
+}
+export const readMessages = createAsyncThunk("chat/read", async ({ sender, receiver }) => {
+    const docId = docIdGenerator(sender, receiver);
+    try {
+        const snapshots = await getDocs(collection(db, "chatroom", docId, "chats"))
+        const chatList = snapshots.docs.map((snap) => snap.data())
+        return chatList;
+    } catch (e) {
+        console.log(e)
+    }
 })
 
-
-export const sendMessage=createAsyncThunk("chat/send",async({message,sender,receiver})=>{
-    const users=[sender,receiver];
-    users.sort()
-    const docId=users[0]+"_"+users[1];
-    
-   const chatId = Date.now().toString();
+export const sendMessage = createAsyncThunk("chat/send", async ({ message, sender, receiver }) => {
+    const docId = docIdGenerator(sender, receiver);
+    const chatId = Date.now().toString();
     try {
-      await setDoc(doc(db, "chatroom", docId, "chats", chatId), {
-        message: message,
-      });
-      return { message };
-    }catch(e){
+        await setDoc(doc(db, "chatroom", docId, "chats", chatId), {
+            chatId: chatId,
+            sender: sender,
+            message: message,
+        });
+        return { message };
+    } catch (e) {
         console.log(e);
     }
 });
 
-const initialstate={
-    chats:[],
-    isLoading:false,
-    error:null,
+export const deleteMessage = createAsyncThunk("chat/delete", async ({ sender, receiver, chatId }) => {
+    const docId = docIdGenerator(sender, receiver);
+    if(sender.email==getCurrentUser.email){}
+    await deleteDoc(doc(db, "chatroom", docId, "chats", chatId))
+})
+
+export const updateMessage = createAsyncThunk(
+  "chat/update",
+  async ({ sender, receiver, chatId, newMessage }) => {
+    const docId = docIdGenerator(sender, receiver);
+
+    await updateDoc(
+      doc(db, "chatroom", docId, "chats", chatId),
+      { message: newMessage }
+    );
+
+    return { newMessage, chatId };
+  }
+);
+
+
+const initialstate = {
+    chats: [],
+    isLoading: false,
+    error: null,
 }
 
-const chatSlice=createSlice({
-    name:"chats",
-    initialState:initialstate,
-    extraReducers:(builder)=>{
-        builder.addCase(sendMessage.pending,(state)=>{
-            state.isLoading=true;
-        }).addCase(sendMessage.fulfilled,(state)=>{
-            state.isLoading=false;
+const chatSlice = createSlice({
+    name: "chats",
+    initialState: initialstate,
+    extraReducers: (builder) => {
+        builder.addCase(sendMessage.pending, (state) => {
+            state.isLoading = true;
+        }).addCase(sendMessage.fulfilled, (state) => {
+            state.isLoading = false;
             //  state.chats.push({ message: message });
             alert("send messages")
-        }).addCase(sendMessage.rejected,(state)=>{
-            state.isLoading=false;
-            state.error="cant send messages"
+        }).addCase(sendMessage.rejected, (state) => {
+            state.isLoading = false;
+            state.error = "cant send messages"
             alert("cant send")
-        }).addCase(readMessages.pending,(state)=>{
-            state.isLoading=true;
-        }).addCase(readMessages.fulfilled,(state,action)=>{
-            state.chats=action.payload
-            state.isLoading=false;
+        }).addCase(readMessages.pending, (state) => {
+            state.isLoading = true;
+        }).addCase(readMessages.fulfilled, (state, action) => {
+            state.chats = action.payload
+            state.isLoading = false;
             console.log("chat fetched successfully")
-        }).addCase(readMessages.rejected,(state)=>{
-            state.isLoading=false;
-            state.error="chat cant fetched !!"
+        }).addCase(readMessages.rejected, (state) => {
+            state.isLoading = false;
+            state.error = "chat cant fetched !!"
             console.log(state.error)
-        })
+        }).addCase(deleteMessage.fulfilled, (state) => {
+            state.error = "message deleted"
+        }).addCase(updateMessage.fulfilled, (state, action) => {
+            const { chatId, newMessage } = action.payload;
+            const index = state.chats.findIndex(c => c.chatId === chatId);
+
+            if (index !== -1) {
+                state.chats[index].message = newMessage;
+            }
+        });
+
+
     }
 })
 
